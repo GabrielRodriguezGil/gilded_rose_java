ae21339bf293add8ff9bb378e6e7e3b8f1e6e8d4
GabrielRodriguezGil <gabrielrodgil088@gmail.com>
2026-03-16 13:58:37 +0100

    chore(proyect): Mover los test de ficheros

--- src/test/java/edu/teamrocket/gilded_rose/domain/ItemTest.java
+++ src/test/java/edu/teamrocket/gilded_rose/domain/ItemTest.java
@@ -1,4 +1,4 @@
-package edu.teamrocket.gilded_rose.domain.domain;
+package edu.teamrocket.gilded_rose.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
